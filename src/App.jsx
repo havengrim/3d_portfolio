3acd0ba57d4d3865a8186571ef1ac1613a5d14cb
Angelo Guiao <angeloguiao.gci@gmail.com>
2024-01-21 16:22:05 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+import { Home, About, Project, Contact } from './pages';
+
+const App = () => {
+  return (
+   <main className='bg-slate-300/20'>
+    <Router>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Project />} />
+            <Route path="/contact" element={<Contact />} />
+        </Routes>
+    </Router>
+   </main>
+  )
+}
+
+export default App
