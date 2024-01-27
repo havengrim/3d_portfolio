9609d146196cdc319452833892b7476e9209a531
Angelo Guiao <angeloguiao.gci@gmail.com>
2024-01-27 20:19:15 +0800

    about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className='max-container'>
+      <h1 className='head-text'>Hello, I'm<span className='blue-gradient_text font-semibold drop-shadow'>Angelo</span></h1>
+    </section>
   )
 }
 
